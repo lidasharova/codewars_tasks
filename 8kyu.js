da76d59964ee3f6aee6cae6c635d78dfdfc21452
lidasharova <Lida050496@yandex.ru>
2023-01-07 22:42:59 +0700

    complete task

--- 8kyu.js
+++ 8kyu.js
@@ -6,3 +6,19 @@ function century(year) {
   return Math.ceil(year / 100);
 }
 
+
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+function even_or_odd(number) {
+  return number % 2 ? 'Odd' : 'Even';
+}
+
+const even_or_odd = (n) => (n % 2 ? 'Odd' : 'Even');
+var even_or_odd = (n) => (n & 1 ? 'Odd' : 'Even');
+const even_or_odd = n => ["Even","Odd"][n&1] ;
+//" [n&1] " в этом случае, если n == четное, оно будет оцениваться как [0], если n == нечетное будет оцениваться [1]
