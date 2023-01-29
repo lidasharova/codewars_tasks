5472430ea5f034d32e8f72830244a1d14c2da451
lidasharova <Lida050496@yandex.ru>
2023-01-29 10:22:35 +0700

    feat:complete task

--- CodewarsPart2/7kyu.js
+++ CodewarsPart2/7kyu.js
@@ -6,23 +6,57 @@ for (i = 0; i < questions.length; i++) {
   questions[i].usersAnswer = null;
 }
 
+
+//1 вариант(не правильный)
+// //task
+// //Вам дан двумерный массив, состоящий из цвета и его «общей» ассоциации в каждом элементе массива. Функция, которую вы напишете, должна возвращать цвет как «ключ» и ассоциацию как «значение».
+
+// function colourAssociation(array) {
+//   let object = {};
+
+//   for (let i = 0; i < array.length; i++) {
+//     //отделяем ключ и значение
+//     let key = array[i][0];
+//     let value = array[i][1];
+
+//     //добавляем в обьект
+//     object[key] = value;
+// }
+
+
+// //из обьекта получаем массив с обьектами
+// const result = Object
+//   .entries(object)
+//   .map(entry => ({[entry[0]]: entry[1]}));
+// return result;
+// }
+
+
+
+//2 вариант(правильный)
 //task
 //Вам дан двумерный массив, состоящий из цвета и его «общей» ассоциации в каждом элементе массива. Функция, которую вы напишете, должна возвращать цвет как «ключ» и ассоциацию как «значение».
 
-function colourAssociation(array) {
-  let object = {};
 
-  for (let i = 0; i < array.length; i++) {
-    //отделяем ключ и значение
-    let key = array[i][0];
-    let value = array[i][1];
+
+function colourAssociation(array){
+let arr = [];
+
+for(let i=0; i<array.length; i++){
+//отделяем ключ и значение
+let key = array[i][0];
+let value = array[i][1];
 
     //добавляем в обьект
-    object[key] = value;
-  }
-  //из обьекта получаем массив с обьектами
-  const result = Object.entries(object).map((entry) => ({
-    [entry[0]]: entry[1],
-  }));
-  return result;
+let object = {};
+object[key] = value;
+console.log(object);
+
+//добавляем в массив
+arr = arr.concat(object);
+console.log(arr);
 }
+
+return arr;
+}
+
